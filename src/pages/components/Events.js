03cf3bae09a03040ea7870960f3955d6534894c6
Crystal <79772390+latsyrc5@users.noreply.github.com>
2023-08-02 15:17:11 -0700

    change event cards to images

--- src/pages/components/Events.js
+++ src/pages/components/Events.js
@@ -3,11 +3,11 @@ import { Grid, Typography, IconButton } from "@mui/material";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import "./../styles/Events.css";
-import chatimeevent from "./../images/chatimeevent.png";
-import pilatesevent from "./../images/pilatesevent.png";
-import cmdfevent from "./../images/cmdfevent.png";
-import workshopevent from "./../images/workshopevent.png";
-import ubcevent from "./../images/ubcevent.png";
+import chatimeevent from "./../images/chatime.png";
+import pilatesevent from "./../images/pilates.png";
+import cmdfevent from "./../images/cmdf.png";
+import workshopevent from "./../images/workshop.png";
+import ubcevent from "./../images/ubc.png";
 import contactevent from "./../images/contactevent.png";
 import leftArrowWhite from "./../images/left-arrow-white.svg";
 import rightArrowWhite from "./../images/right-arrow-white.svg";
@@ -22,25 +22,19 @@ export default function Events() {
     contactevent,
   ];
 
-  const eventTitles = [
-    "Upcoming Event",
-    "A Pilates ParTEA",
-    "Balancing Anything and",
-    "Intro to Design Thinking",
-    "STEAM Through College & Beyond",
-    "",
-  ];
-
   const [activeIndex, setActiveIndex] = useState(0);
+  const [showBackButton, setShowBackButton] = useState(false);
 
   const handleNextClick = () => {
     setActiveIndex((prevIndex) => (prevIndex + 1) % eventImages.length);
+    setShowBackButton(true);
   };
 
   const handlePrevClick = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? eventImages.length - 1 : prevIndex - 1
+      prevIndex === 0 ? 0 : prevIndex - 1
     );
+    setShowBackButton(activeIndex !== 1);
   };
 
   return (
@@ -60,49 +54,33 @@ export default function Events() {
         </Typography>
       </Grid>
       <Grid item xs={12} sm={12} className="events">
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <IconButton
-            style={{ color: "#FFFFFF" }}
-            aria-label="Previous"
-            onClick={handlePrevClick}
-            className="alice-carousel__prev-btn"
-          >
-            <img src={rightArrowWhite} alt="Previous" />
-          </IconButton>
-          <Typography
-            variant="body1"
-            sx={{
-              fontFamily: "Josefin Sans",
-              color: "#FFFFFF",
-              textAlign: "left",
-            }}
-          >
-            BACK
-          </Typography>
-        </div>
+        {showBackButton && (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <IconButton
+              style={{ color: "#FFFFFF" }}
+              aria-label="Previous"
+              onClick={handlePrevClick}
+              className="alice-carousel__prev-btn"
+            >
+              <img src={rightArrowWhite} alt="Previous" />
+            </IconButton>
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: "Josefin Sans",
+                color: "#FFFFFF",
+                textAlign: "left",
+              }}
+            >
+              BACK
+            </Typography>
+          </div>
+        )}
+
         <AliceCarousel
           items={eventImages.map((imgSrc, index) => (
             <div key={index} className="shadow-rectangle-events">
-              <div className="events-photo-wrapper">
-                <img
-                  src={imgSrc}
-                  alt={`Event ${index + 1}`}
-                  className="photo"
-                />
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontFamily: "Josefin Sans",
-                    color: "black",
-                    marginTop: "15px",
-                    marginLeft: "15px",
-                    textAlign: "left",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {eventTitles[index]}
-                </Typography>
-              </div>
+              <img src={imgSrc} alt={`Event ${index + 1}`} className="photo" />
             </div>
           ))}
           responsive={{
@@ -115,6 +93,7 @@ export default function Events() {
           disableButtonsControls
           activeIndex={activeIndex}
         />
+
         <div style={{ display: "flex", flexDirection: "column" }}>
           <IconButton
             style={{ color: "#FFFFFF" }}
