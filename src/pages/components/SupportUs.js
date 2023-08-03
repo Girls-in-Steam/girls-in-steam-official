9ccca7bdcfe74c11ad293f0afec21c885a58bb5e
Britney So <britneyso@Britneys-MacBook-Pro.local>
2023-08-03 11:16:08 -0700

    Gradient Button

--- src/pages/components/SupportUs.js
+++ src/pages/components/SupportUs.js
@@ -4,20 +4,23 @@ import steambbt from "./../images/steambbt.png";
 import "./../styles/AboutUs.css";
 import "./../../fonts/JosefinSans-VariableFont_wght.ttf";
 import "./../../fonts/Rastano.ttf";
+import thermoPlaceHolder from "./../images/thermo.png";
 
 export default function SupportUs() {
   return (
-    <Grid container spacing={2} className="container">
+    <Grid container spacing={2} className="container" >
       <Grid item xs={12} sm={6}>
-        <div sx={{ maxWidth: 400 }}>
+        <div style={{ maxWidth: 600}}>
           <Typography
             variant="h4"
             gutterBottom
             sx={{
               textAlign: "left",
-              color: "#6363AB",
+              color: "#ffffff",
               fontFamily: "Josefin Sans",
               fontWeight: "bold",
+              marginBottom: "36px",
+
             }}
           >
             SUPPORT US
@@ -27,9 +30,10 @@ export default function SupportUs() {
             gutterBottom
             sx={{
               textAlign: "left",
-              color: "#6363AB",
+              marginLeft: "24px",
+              marginBottom: "36px",
+              color: "#54547E",
               fontFamily: "Josefin Sans",
-              fontWeight: "bold",
             }}
           >
             Girls in STEAM appreciates your support.
@@ -38,20 +42,20 @@ export default function SupportUs() {
             variant="h6"
             sx={{
               textAlign: "left",
-              color: "#5A4067",
+              color: "#54547E",
               fontFamily: "Josefin Sans",
-              fontWeight: "bold",
+              marginLeft: "24px",
               marginBottom: "70px",
             }}
           >
-            We aim to help balance the gender disparity in the tech industry
-            through creating community, educational programming, and an
-            accessible network for girls, women, gender-diverse* people, and the
-            LGBTQ+ community to kick start their early careers.
+            Join us in our journey to support access to STEAM education 
+            for girls, women, gender-diverse* people, and the LGBT+ 
+            community across North America.
           </Typography>
         </div>
       </Grid>
       <Grid item xs={12} sm={6}>
+        {/*
         <div className="shadow-rectangle">
           <div className="photo-wrapper">
             <img src={steambbt} alt="Girls in STEAM Exec team" className="photo" />
@@ -67,8 +71,41 @@ export default function SupportUs() {
             </Typography>
           </div>
         </div>
+        */}
+        <div style={{
+          minHeight: '400px',
+          minWidth: '200px'
+        }}>
+          <img src={thermoPlaceHolder} alt="image not found" style={{maxWidth: "220px"}}/>
+        </div>
+        <div>
+          <button style={{
+            minWidth: "250px", 
+            minHeight: '80px', 
+            backgroundColor: '#54547E', 
+            borderRadius: '10px', 
+            border: 'none', 
+            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',}}
+            onClick={() => {window.open('https://example.com', '_blank')}}>
+            <div> 
+              <Typography
+                  variant="h4"
+                  sx={{
+                    fontFamily: "Josefin Sans",
+                    color: "white",
+                    marginTop: "8px",
+                    fontWeight: "bold",
+                    padding: '20px'
+                  }}
+                >
+                DONATE
+              </Typography>
+            </div>
+          </button>
+        </div>
       </Grid>
-      <Grid item xs={12} sx={{ margin: "0 2", backgroundColor: "white" }}>
+      {/*
+    <Grid item xs={12} sx={{ margin: "0 2", backgroundColor: "green" }}>
         <Typography
           variant="h6"
           gutterbottom
@@ -84,6 +121,8 @@ export default function SupportUs() {
         </Typography>
         <img src={map1} alt="Map" style={{ width: "70%", height: "auto" }} />
       </Grid>
+            */}
+      
     </Grid>
   );
 }
