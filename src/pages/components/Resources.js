61b8a163e9af6c8c93f35c94557a1e2a4b97e443
Britney So <britneyso@Britneys-MacBook-Pro.local>
2023-10-18 03:14:37 -0700

    added resource boxes and hashtags

--- src/pages/components/Resources.js
+++ src/pages/components/Resources.js
@@ -1,22 +1,66 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Link } from "@mui/material";
+import { RESOURCE_DATA } from './../data/ResourceData'
+import "../styles/Resources.css";
 
-export default function SupportUs() {
+function ResourceBox(resourceDescrption, link) {
     return(
-        <div id="resource-title">
-                <Box sx={{ 
-                            display: 'flex', 
-                            flexDirection: 'row-reverse',
-                            marginRight: '10px'
-                    }}>
-                    <Typography
-                        variant="h4"
-                        sx={{
-                            fontWeight: "bold",
-                        }}
-                    >
-                        RESOURCES
-                    </Typography>
-                </Box>
+        <div className = "resource-box">
+            <Typography variant="body1" sx={{color: 'black'}}>
+                {resourceDescrption}
+            </Typography>
+            <Typography variant="body2" sx={{color: 'black'}}>
+                {link}
+            </Typography>
+        </div>
+    )
+}
+
+function ResourceColumn(resourceData) {
+    let resources = [];
+    let columnTag = resourceData[0]; //gets hashtag from first element
+    for(let i=1 ; i<resourceData.length ; i++){
+        resources.push(
+            ResourceBox(resourceData[i][0], resourceData[i][1])
+        )
+    }
+
+    return(
+        <div className = "resource-column">
+            <div className="column-hastag">
+                <Typography variant="body1">
+                    {columnTag}
+                </Typography>
+            </div>
+            {resources}
+        </div>
+    )
+}
+
+export default function Resources() {
+    let resourceColumns = []
+    for(let i=0 ; i<RESOURCE_DATA.length ; i++){
+        resourceColumns.push(ResourceColumn(RESOURCE_DATA[i]))
+    }
+
+    return(
+        <div className="resources-container">
+            <Box sx={{ 
+                    display: 'flex', 
+                    flexDirection: 'row-reverse',
+                    marginRight: '10px'
+                }}>
+                <Typography
+                    variant="h4"
+                    sx={{
+                        fontWeight: "bold",
+                    }}
+                >
+                    RESOURCES
+                </Typography>
+            </Box>
+            <div className = "columns-container">
+                {resourceColumns}
+            </div>
         </div>
     )
 }
---
+++ src/pages/data/ResourceData.js
@@ -0,0 +1,19 @@
+export const RESOURCE_DATA = [
+    [
+        '#InfoFriday', 
+        ['Women Who Code', 'https://www.womenwhocode.com/'],
+        ["ada's List", 'https://www.adaslist.co/'],
+        ['Colorstack', 'https://www.colorstack.org/'],
+        ['Rewiriting the Code', 'https://girlsinsteam.notion.site/Rewriting-the-Code-376990dd78d041f89d0ab4e2dfd06c26?pvs=25'],
+        ['Reboot', 'https://reboothq.substack.com/']
+    ],
+    [
+        '#SundayScoop',
+    ],
+    [
+        '#ThoughfulThursday'
+    ],
+    [
+        '#Other'
+    ]
+]
