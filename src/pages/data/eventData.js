2cab08b2b69bef5f84f5b66cd361eb02f560297e
Crystal <79772390+latsyrc5@users.noreply.github.com>
2023-10-30 15:02:54 -0700

    change event titles to text instead of images

--- src/pages/data/eventData.js
+++ src/pages/data/eventData.js
@@ -1,33 +1,44 @@
-import chatimeevent from "./../images/chatime.png";
-import pilatesevent from "./../images/pilates.png";
-import cmdfevent from "./../images/cmdf.png";
-import workshopevent from "./../images/workshop.png";
-import ubcevent from "./../images/ubc.png";
+import chatimeevent from "./../images/chatimeevent.png";
+import pilatesevent from "./../images/pilatesevent.png";
+import cmdfevent from "./../images/cmdfevent.png";
+import workshopevent from "./../images/workshopevent.png";
+import ubcevent from "./../images/ubcevent.png";
 import contactevent from "./../images/contactevent.png";
 
 export const eventData = [
-    {
-      imgSrc: chatimeevent,
-      instagramLink: "https://www.instagram.com/p/CtAy1ZcrlM9/",
-    },
-    {
-      imgSrc: pilatesevent,
-      instagramLink: "https://www.instagram.com/p/CsT2JoHr7bZ/?img_index=1",
-    },
-    {
-      imgSrc: cmdfevent,
-      instagramLink: "https://www.instagram.com/p/CptFHvhyxmv/?img_index=1",
-    },
-    {
-      imgSrc: workshopevent,
-      instagramLink: "https://www.instagram.com/p/CqMFRo6vlJc/",
-    },
-    {
-      imgSrc: ubcevent,
-      instagramLink: "https://www.instagram.com/p/CklpXJcJQzL/?img_index=1",
-    },
-    {
-      imgSrc: contactevent,
-      instagramLink: "mailto:info@girlsinsteam.org",
-    },
-  ];
+  {
+    title: "Chatime Event",
+    description: "Past Event",
+    imgSrc: chatimeevent,
+    instagramLink: "https://www.instagram.com/p/CtAy1ZcrlM9/",
+  },
+  {
+    title: "A Pilates ParTEA",
+    description: "Past Fundraiser",
+    imgSrc: pilatesevent,
+    instagramLink: "https://www.instagram.com/p/CsT2JoHr7bZ/?img_index=1",
+  },
+  {
+    title: "Balancing Anything & Everything",
+    description: "Past Hackathon Workshop",
+    imgSrc: cmdfevent,
+    instagramLink: "https://www.instagram.com/p/CptFHvhyxmv/?img_index=1",
+  },
+  {
+    title: "Intro to Design Thinking",
+    description: "Past Virtual Workshop",
+    imgSrc: workshopevent,
+    instagramLink: "https://www.instagram.com/p/CqMFRo6vlJc/",
+  },
+  {
+    title: "STEAM Through College & Beyond",
+    description: "Past Panel Event",
+    imgSrc: ubcevent,
+    instagramLink: "https://www.instagram.com/p/CklpXJcJQzL/?img_index=1",
+  },
+  {
+    title: "Contact Us",
+    imgSrc: contactevent,
+    instagramLink: "mailto:info@girlsinsteam.org",
+  },
+];
