5e75c403b918dd03dc0c8b5007dc1860ce0202cb
Marina Wang <marina.wjn@gmail.com>
2024-04-03 15:56:44 -0700

    updated questions and answers for the FAQ section

--- src/pages/data/FAQData.js
+++ src/pages/data/FAQData.js
@@ -1,14 +1,18 @@
 export const FAQData = [
     {
         question: "What does STEAM stand for?",
-        answer: "STEAM stands for Science, Technology, Engineering, Art & Design, Maths",
+        answer: "Many people are familiar with the acronym STEM (Science, Technology, Engineering, Maths), which represents a style of education that focuses on emphasizing how each discipline in STEM connects with each other with technology. STEAM builds on top of STEM by adding an A which represents Arts subjects, that encompasses areas such as design and creativity. STEAM puts the artistic way of thinking front and centre, and encourages students to solve problems interdiscplinally.",
     },
     {
         question: "How do I learn more about GIS?",
-        answer: "Follow our instagram @girlsinsteamorg",
+        answer: "Stay updated on networking events, information sessions, giveaways, and so much more by following our Instagram @girlsinsteamorg!",
     },
     {
         question: "How do I join the GIS team?",
-        answer: "Recruitment for the executive team happens every year in January. Stay tuned!",
-    } 
+        answer: "Recruitment for the executive team happens every year in January. There is no restriction on gender for our recruitment, all genders are welcome as long as you have a passion for STEAM!",
+    },
+    {
+        question: "What are some perks and benefits of joining the GIS team?",
+        answer: "Not only will you gain experience in digital marketing and event planning, but you'll also have access to networking and mentorship opportunities. As a campus ambassador, you'll also enjoy monthly prizes and volunteer hours!",
+    },
 ];
